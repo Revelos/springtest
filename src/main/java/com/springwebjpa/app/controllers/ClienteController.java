2ef1135473b46e5e9c3a9096f9ae6d2227fe4ee8
Revelos <cristianrevelo98@gmail.com>
2022-03-05 20:14:11 -0600

    Se modifico controller

--- src/main/java/com/springwebjpa/app/controllers/ClienteController.java
+++ src/main/java/com/springwebjpa/app/controllers/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.springwebjpa.app.models.entity.Cliente;
 import com.springwebjpa.app.models.service.IClienteService;
 
-@RestController
+@CrossOrigin(origins = "*")
+@Controller
+@RequestMapping("/")
 @SessionAttributes("cliente")
 public class ClienteController {
 
